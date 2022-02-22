ebcb36b0b84938b4b51701b3be7aa03c9cdf35b7
Mehedi Hassan <howlader.mehedihassan@gmail.com>
2022-02-22 09:26:24 +0600

    feat: run invisible html window with a python file

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 // Global reference to mainWindow
 let mainWindow;
+let invisibleWindow;
 
 app.on("ready", function () {
     // Create new window
@@ -28,6 +29,33 @@ app.on("ready", function () {
     // Creating the Menu
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     Menu.setApplicationMenu(mainMenu);
+});
+
+function createInvisibleWindow() {
+    invisibleWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        show: false,
+    });
+
+    invisibleWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, "invisible.html"),
+            protocol: "file:",
+            slashes: true,
+        })
+    );
+}
+
+ipcMain.on("generate-random-number", function (event) {
+    createInvisibleWindow();
+});
+
+ipcMain.on("random-number", function (event, number) {
+    mainWindow.webContents.send("random-number", number);
+    invisibleWindow.close();
 });
 
 // Create menu template
